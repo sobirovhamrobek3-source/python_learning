0f69c28c2b66dc6e32f5433b0df77006abcc49b2
sobirovhamrobek3-source <sobirovhamrobek3@gmail.com>
2025-11-13 18:11:22 +0500

    Create hw_1.py

---
+++ lesson-1/homework/hw_1.py
@@ -0,0 +1,19 @@
+a = int(input("enter height of square: "))
+b = int(input("enter width of square: "))   
+print(a*b)
+p = 2*(a+b)
+print(p)   
+import math
+d = float(input("enter diameter of circle: "))
+L = math.pi*d
+print(("enter diameter of circle"),L)
+a = 4
+b = 6
+mean = (a+b)/2
+print(mean)
+a = 25
+b = 35
+print( a+ b)
+print(a * b)
+print(a * 25)
+print( b* 35)
